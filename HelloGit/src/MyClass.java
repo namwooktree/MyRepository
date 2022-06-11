c2356b1e14b03b7b6f61a1f3d2d2379749deb8fa
남우 <@LAPTOP-TC7G0RF9>
2022-06-11 11:03:47 +0900

    Change in master after branching dev

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -3,5 +3,6 @@ public class MyClass {
 	public static void main(String args[]) {
 		System.out.println("Hello Git!");
 		System.out.println("This is 1.0 version.");
+		System.out.println("Change in master branch");
 	}
 }
