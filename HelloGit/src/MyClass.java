b754ce6fceacb009463e7630eec43ff030104b77
남우 <@LAPTOP-TC7G0RF9>
2022-06-11 10:56:47 +0900

    First commit in dev branch

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -3,5 +3,7 @@ public class MyClass {
 	public static void main(String args[]) {
 		System.out.println("Hello Git!");
 		System.out.println("This is 1.0 version.");
+		MyHello myHello = new MyHello();
+		myHello.sayHello();
 	}
 }
